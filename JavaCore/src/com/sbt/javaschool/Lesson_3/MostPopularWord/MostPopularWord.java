3bfdc821211ea2093d874e3346fc52144d90e2aa
TigranMin <mintigran@gmail.com>
2019-11-07 15:13:28 +0300

    Lesson_3: Task 2056 - Most popular word

--- JavaCore/src/com/sbt/javaschool/Lesson_3/MostPopularWord/MostPopularWord.java
+++ JavaCore/src/com/sbt/javaschool/Lesson_3/MostPopularWord/MostPopularWord.java
@@ -10,51 +10,42 @@ import java.util.TreeSet;
 
 public class MostPopularWord {
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader output = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader reader = new BufferedReader(new FileReader(input.readLine()));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(output.readLine()));
+        BufferedReader reader = new BufferedReader(new FileReader("D:/Java/input.txt"));
+        //BufferedWriter writer = new BufferedWriter(new FileWriter("D:/Java/output.txt"));
 
         ArrayList<String> list = new ArrayList<String>();
         String s;
-        try {
-            while (true){
-                s = reader.readLine();
-                if(s == null){
+
+        while (true){
+            s = reader.readLine();
+            if(s == null){
                     break;
-                }
-                for (String word: s.split("\\s+")) {
-                    list.add(word.toLowerCase());
-                }
             }
-            int maxFreq = 0;
-            for (String word: list) {
-                if(Collections.frequency(list, word) > maxFreq){
-                    maxFreq = Collections.frequency(list, word);
-                }
+            for (String word: s.split("\\s+")) {
+                list.add(word.toLowerCase());
             }
-
-            TreeSet<String> set = new TreeSet<>();
-
-            for (String word: list) {
-                if(Collections.frequency(list, word) == maxFreq){
-                    //System.out.println(word);
-                    set.add(word);
-                }
-            }
-            for (String mostpopwords : set) {
-                writer.write(mostpopwords);
-                writer.newLine();
-            }
-
-            reader.close();
-            writer.close();
         }
-        catch (Exception e){
-            reader.close();
-            writer.close();
+        int maxFreq = 0;
+        for (String word: list) {
+            if(Collections.frequency(list, word) > maxFreq){
+                maxFreq = Collections.frequency(list, word);
+            }
         }
 
+        TreeSet<String> set = new TreeSet<>();
+
+        for (String word: list) {
+            if(Collections.frequency(list, word) == maxFreq){
+                set.add(word);
+            }
+        }
+        for (String mostpopwords : set) {
+//               writer.write(mostpopwords);
+//               writer.newLine();
+            System.out.println(mostpopwords);
+        }
+        reader.close();
+        //writer.close();
     }
 }
