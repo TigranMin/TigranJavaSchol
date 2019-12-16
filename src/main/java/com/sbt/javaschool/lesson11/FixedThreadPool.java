9ee11255b9388622e35526939a221c48e59920da
TigranMin <mintigran@gmail.com>
2019-12-16 11:46:12 +0300

    lesson11 - some changes to FixedThreadPool

--- src/main/java/com/sbt/javaschool/lesson11/FixedThreadPool.java
+++ src/main/java/com/sbt/javaschool/lesson11/FixedThreadPool.java
@@ -1,21 +1,27 @@
 package com.sbt.javaschool.lesson11;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class FixedThreadPool implements ThreadPool {
     private final Queue<Runnable> taskQueue = new ConcurrentLinkedQueue<>();
+    private final List<Thread> threadList = new ArrayList<>();
+
     private volatile boolean isRunning = true;
 
     public FixedThreadPool(int threadsCount) {
         for (int i = 0; i < threadsCount; i++) {
-            new Thread(new Task(), "#" + i).start();
+            threadList.add(new Thread(new Task(), "#" + i));
         }
     }
 
     @Override
     public void start() {
-
+        for (Thread t: threadList) {
+            t.start();
+        }
     }
 
     @Override
@@ -25,7 +31,7 @@ public class FixedThreadPool implements ThreadPool {
         }
     }
 
-    public void shutdown() {
+    private void shutdown() {
         isRunning = false;
     }
 
@@ -33,6 +39,9 @@ public class FixedThreadPool implements ThreadPool {
 
         @Override
         public void run() {
+            if (threadList.size() > taskQueue.size()){
+                shutdown();
+            }
             while (isRunning) {
                 Runnable nextTask = taskQueue.poll();
                 if (nextTask != null) {
